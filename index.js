380819f0d19ffa1819c4173866442d9dd41870fe
Yaazieli <yaazieli@gmail.com>
2021-10-17 20:19:44 -0400

    commit tree 4d8bd1af0242ae66a4dd4f49cf22b06755cb6da2

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetnumber){
+   if (feetnumber <= 400) {
+     return 'This one is on me!';
+   } else if (feetnumber > 2000 && feetnumber < 2500) {
+      return 'I will gladly take your thirty bucks.'
+   } else if (feetnumber > 2500) {
+     return 'No can do.'
+   }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(specifyCity){
+  return (specifyCity === 'NYC') ? 'Ok, sounds good.' : 'No go.'   
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  if (generosity === "generous" )
+  if (generosity === "not as generous" )
+  if (generosity === "thanks for everything" )
+  return "Thank you so much."
+
+  switch(generosity) {
+    case "generous":
+    return "Thank you so much."
+    break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    case "thanks for everything":
+      return "Bye."
+
+  }
 }
